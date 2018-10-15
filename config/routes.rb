a5d7585fa421a00b0c31cb48ac3805989fbf9f88
wimbleclaw <aisha305@hotmail.co.uk>
2018-10-15 10:51:49 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: %i[index show new create edit update]
+
+  # get 'post/:id/edit', to: 'post#edit', as: :edit_post
+  # patch 'posts/:id', to: 'posts#update'
 end
